f96fd914f2e5c2502e522317c0bcf3fbee1a5b7a
algosalih <algosalih@gmail.com>
2025-04-12 17:13:09 +0300

    Created the admin order management page

---
+++ src/types/Order.ts
@@ -0,0 +1,30 @@
+// import
+import { Menu } from './Menu';
+import { Restaurant } from './Restaurant';
+import { CourierDTO } from './Courier';
+
+
+export interface OrderDTO {
+    pk: number;
+    name: string;
+    restaurant: Restaurant;
+    menus: Menu[];
+    courier: CourierDTO | null;
+    status: OrderStatusEnum;
+    courierAssignmentAccepted: boolean;
+    version: number;
+    totalPrice: number;
+    searchString?: string; // Added for client-side search
+  }
+  
+export enum OrderStatusEnum {
+  PLACED = 'PLACED',
+  CONFIRMED = 'CONFIRMED',
+  PREPARING = 'PREPARING',
+  READY_FOR_PICKUP = 'READY_FOR_PICKUP',
+  ASSIGNED = 'ASSIGNED',
+  IN_TRANSIT = 'IN_TRANSIT',
+  DELIVERED = 'DELIVERED',
+  CANCELLED = 'CANCELLED',
+  REJECTED = 'REJECTED',
+}
